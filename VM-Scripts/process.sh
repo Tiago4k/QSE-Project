655a5a481163f512b05d6eae3d9007041fd1ef66
Tiago <tiago4k@gmail.com>
2020-01-03 18:31:35 +0000

    More fixes

--- VM-Scripts/process.sh
+++ VM-Scripts/process.sh
@@ -3,7 +3,6 @@
 # Script to enable services at VM start up, and start them.
 
 sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable elasticsearch.service
-sudo /bin/systemctl start elasticsearch.service
-sudo /bin/systemctl enable kibana.service
-sudo /bin/systemctl start kibana.service
+sudo /bin/systemctl enable elasticsearch.service && sudo /bin/systemctl start elasticsearch.service
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable kibana.service && sudo /bin/systemctl start kibana.service
