903445ec442c2c865f1b83b0daf5d8d2e5086532
Tiago <tiago4k@gmail.com>
2020-01-03 18:05:28 +0000

    More fixes

--- VM-Scripts/Elasticsearch-Kibana-Installation.sh
+++ VM-Scripts/Elasticsearch-Kibana-Installation.sh
@@ -14,34 +14,13 @@ echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee 
 sudo apt-get update && sudo apt-get install -yy elasticsearch
 echo "deb https://artifacts.elastic.co/packages/oss-7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 
-# Configure elasticsearch.yml with:
-
-###### some process.sh file #######
-
-# sudo echo "node.name: es-vm" >> /etc/elasticsearch/elasticsearch.yml
-# sudo echo "network.host: "0.0.0.0""  >> /etc/elasticsearch/elasticsearch.yml
-# sudo echo	"discovery.seed_hosts: ["127.0.0.1"]"  >> /etc/elasticsearch/elasticsearch.yml
-# sudo echo	"cluster.initial_master_nodes: ["es-vm"]" >> /etc/elasticsearch/elasticsearch.yml
-
-
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable elasticsearch.service
-sudo /bin/systemctl start elasticsearch.service
-
-
 # Download and install Kibana
 
 sudo apt-get install -yy kibana
 sudo vi /etc/kibana/kibana.yml
 
-# Configure kibana.yml with:
+# Configure elasticsearch.yml with elastic-config.yml contents
+sudo vim /etc/elasticsearch/elasticsearch.yml
 
-###### some process.sh file #######
-
-# sudo echo	"server.port: "5601"" >> /etc/kibana/kibana.yml
-# sudo echo	"server.host: “0.0.0.0”" >> /etc/kibana/kibana.yml
-# sudo echo	"elasticsearch.hosts: ["http://127.0.0.1:9200"]" >> /etc/kibana/kibana.yml
-
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable kibana.service
-sudo /bin/systemctl start kibana.service
+# Configure kibana.yml with kibana-config.yml contents
+sudo vim /etc/kibana/kibana.yml
--- VM-Scripts/process.sh
+++ VM-Scripts/process.sh
@@ -1,23 +1,9 @@
-export node_name="es-vm"
-export network_host="0.0.0.0"
-export seed_host="127.0.0.1"
-export master_node="es-vm"
-export server_port="5601"
-export server_host="0.0.0.0"
-export elastic_host="http://127.0.0.1:9200"
+#!/bin/bash
 
-sudo rm -f /etc/elasticsearch/elasticsearch.yml temp.yml
-( echo "cat &lt;&lt;EOF &gt;elasticsearch.yml";
-  cat temp-elastic.yml;
-  echo "EOF";
-) >temp.yml
-. temp.yml
-sudo cat /etc/elasticsearch/elasticsearch.yml
+# Script to enable services at VM start up, and start them.
 
-sudo rm -f /etc/kibana/kibana.yml temp.yml
-( echo "cat &lt;&lt;EOF &gt;kibana.yml";
-  cat temp-kibana.yml;
-  echo "EOF";
-) >temp.yml
-. temp.yml
-sudo cat /etc/kibana/kibana.yml
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable elasticsearch.service
+sudo /bin/systemctl start elasticsearch.service
+sudo /bin/systemctl enable kibana.service
+sudo /bin/systemctl start kibana.service
