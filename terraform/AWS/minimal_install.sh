df1a7335e6aa655c7f0628e334e24fef7863ea14
Tiago <tiago4k@gmail.com>
2020-01-04 01:51:11 +0000

    Remove commands

--- terraform/AWS/minimal_install.sh
+++ terraform/AWS/minimal_install.sh
@@ -5,5 +5,4 @@ sudo apt install -yy python2.7 python-pip
 git clone https://github.com/Tiago4k/QSE-Project.git
 cd QSE-Project/scripts
 sh elk-packages.sh
-cd /opt
-sudo sh /QSE-Project/scripts/storm-package.sh
+cd ~
